a281d8f9d952a9f15855e41623291cb3dc6110a1
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-24 15:02:27 +0100

    Working on nav-bar

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- https://policies.google.com/faq -->
+
+<?php
+	echo 'hello world';
+
+	$db = [
+		// code
+	];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./style.css">
+	<title>php-google-faq</title>
+</head>
+<body>
+	<h1>php-google-faq</h1>
+</body>
+</html>
